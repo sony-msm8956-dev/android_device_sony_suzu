04c8bd8b9156eac5183886ac07412341e86b6464
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2022-09-14 14:40:33 +0200

    suzu: Set TARGET_BOOTANIMATION_HALF_RES to true
    * No apparent resolution difference
    * Much smoother animation
    * Much faster boot
    * What's not to like?
    
    Change-Id: I2119fe826146a3103da50b4862ad88f5950a97ec

--- lineage_suzu.mk
+++ lineage_suzu.mk
@@ -26,6 +26,7 @@ $(call inherit-product, device/sony/suzu/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot animation
+TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
